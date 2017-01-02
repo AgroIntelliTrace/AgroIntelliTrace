215e09919aead79d4a095a2605f88b41c3e88ca5
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2017-01-02 14:09:34 -0600

    Carga de Proyecto
    Iniciacion del Proyecto

---
+++ Software/CapadeDatos/Conexion/ConexionBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace CapadeDatos
+{
+    public class ConexionBase
+    {
+
+        protected string cadenaConexion = ConexionSQL.LeerConexion();
+        public bool Exito { get; set; }
+        public string Mensaje { get; set; }
+        public DataTable Datos { get; set; }
+        protected  Conexion _conexion = new Conexion(ConexionSQL.LeerConexion());
+
+    }
+}
---
+++ Software/CapadeDatos/Conexion/UtileriasCodigo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapadeDatos
+{
+    public enum EnumTipoDato
+    {
+        CadenaTexto,
+        Entero,
+        Fecha,
+        Tipodecimal,
+        Boleano,
+        imagen,
+        EnteroLargo
+    }
+}
